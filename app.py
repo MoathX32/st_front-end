882cb36904d2eef35160035a5abed3a9d552c031
Moath Mohamed <moathx32@gmail.com>
2024-11-13 23:27:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -150,9 +150,7 @@ if st.sidebar.button("مسح الجلسات"):
     else:
         st.error("فشل في مسح الجلسات.")
 
-st.sidebar.header(
-<div class='sidebar-rtl'>
-":طريقة الاستخدام"</div>)
+st.sidebar.header(":طريقة الاستخدام")
 # الشريط الجانبي - إرشادات الاستخدام والتنويه
 st.sidebar.markdown("""
 <div class='sidebar-rtl'>
